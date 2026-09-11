68ea1ec436ef207d97b47d1598caa869f25d7960
M Defryan <muhammaddefryan@gmail.com>
2026-09-11 11:21:12 +0700

    day03: react router

--- intro-react/src/main.tsx
+++ intro-react/src/main.tsx
@@ -2,10 +2,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Home from './Home.tsx';
 import ProfilePage from './pages/profile';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+const router = createBrowserRouter([
+    {
+        path: '/', 
+        element: <Home />
+    }, 
+    {
+        path: '/profile', 
+        element: <ProfilePage />
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
-    <>
-        <Home />
-        <ProfilePage />
-    </>
+    <RouterProvider router={router} />
 );
