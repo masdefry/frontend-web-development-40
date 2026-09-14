c670ec807ebe5b1d9fe5f91ad945151f47fc324a
M Defryan <muhammaddefryan@gmail.com>
2026-09-14 09:05:41 +0700

    day03: done

--- intro-react/src/Home.tsx
+++ intro-react/src/Home.tsx
@@ -2,12 +2,17 @@
   Class
   Function
 */
+import { Link } from 'react-router';
 import './Home.css';
 function Home() {
   return (
     <>
       <h1 id='title'>Home Page</h1>
       <button>Submit</button>
+      <Link id='' to='/profile'>
+        <button>Go to Profile</button>
+      </Link>
+      <a href='/profile'>Goto Profile Page HREF</a>
     </>
   );
 }
--- intro-react/src/pages/profile/index.tsx
+++ intro-react/src/pages/profile/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router';
 import style from './index.module.css';
 export default function ProfilePage() {
   return (
     <>
       <h1 style={{ color: 'red', backgroundColor: 'black' }}>Profile Page</h1>
       <h2 className={style.bgRed}>Defryan</h2>
+      <Link to='/'>Goto Home</Link>
     </>
   );
 }
